1b850586b93e8c53dcd2f47d1838d4749e178c7a
Yahav <Yahav@users.noreply.github.com>
2022-12-27 18:22:26 +0200

    Modified presence channels members class
    Modified presence channels members functionality since we do not keep users id's\info locally

--- src/core/channels/members.ts
+++ src/core/channels/members.ts
@@ -1,5 +1,11 @@
 import * as Collections from '../utils/collections';
 
+/**
+ *  @Yahav - Caster.fm Cloud
+ *  Overwrite the Members class so we don't store members info locally for scalability
+ *  We'll use the Soketi http api for users list
+ */
+
 /** Represents a collection of members of a presence channel. */
 export default class Members {
   members: any;
@@ -19,6 +25,9 @@ export default class Members {
    * @return {Object} member's info or null
    */
   get(id: string): any {
+    
+    return null; // @Yahav:Added
+
     if (Object.prototype.hasOwnProperty.call(this.members, id)) {
       return {
         id: id,
@@ -46,28 +55,37 @@ export default class Members {
 
   /** Handles subscription data. For internal use only. */
   onSubscription(subscriptionData: any) {
-    this.members = subscriptionData.presence.hash;
+    this.members = {}; // @Yahav: Added
+    //this.members = subscriptionData.presence.hash; // @Yahav: Commented
     this.count = subscriptionData.presence.count;
     this.me = this.get(this.myID);
   }
 
   /** Adds a new member to the collection. For internal use only. */
   addMember(memberData: any) {
-    if (this.get(memberData.user_id) === null) {
-      this.count++;
-    }
-    this.members[memberData.user_id] = memberData.user_info;
-    return this.get(memberData.user_id);
+     // @Yahav:Added the following
+    this.count++;
+    return memberData;
+    // @Yahav: Commented the following
+    // if (this.get(memberData.user_id) === null) {
+    //   this.count++;
+    // }
+    // this.members[memberData.user_id] = memberData.user_info;
+    // return this.get(memberData.user_id);
   }
 
   /** Adds a member from the collection. For internal use only. */
   removeMember(memberData: any) {
-    var member = this.get(memberData.user_id);
-    if (member) {
-      delete this.members[memberData.user_id];
-      this.count--;
-    }
-    return member;
+   // @Yahav:Added the following
+    this.count--;
+    return memberData;
+    // @Yahav: Commented the following
+    // var member = this.get(memberData.user_id);
+    // if (member) {
+    //   delete this.members[memberData.user_id];
+    //   this.count--;
+    // }
+    // return member;
   }
 
   /** Resets the collection to the initial state. For internal use only. */
